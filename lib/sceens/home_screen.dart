bf3dd91f3d1df3badb72423f2191e80f0d27dd32
bakermrad <bakermrad0@gmail.com>
2023-11-16 00:13:58 +0200

    splash screen in work

---
+++ lib/sceens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/semantics.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bocchiColor = Color.fromARGB(255, 255, 71, 159);
+  static const Color ryoColor = Color.fromARGB(255, 69, 109, 249);
+  static const Color kitaColor = Color.fromARGB(255, 250, 21, 21);
+  static const Color nijikaColor = Color.fromARGB(255, 249, 231, 69);
+  static const Color backgroundColor = Color.fromARGB(255, 28, 28, 28);
+  static const Color textColor = Color(0xFFFFFFFF);
+}
